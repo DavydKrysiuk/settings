354a33aefccd53ba1f1cf6e41ca350d4e77fe65d
Davyd Krysiuk <davydkrysiuk@gmail.com>
2024-07-26 23:39:10 +0100

    Update setup.sh for golang installation

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
+# Remove all preinstalled dogshit
+
 sudo dnf remove firefox gnome-tour gnome-contacts gnome-maps gnome-calendar gnome-connections gnome-weather libreoffice-calc libreoffice-impress libreoffice-writer libreoffice-core;
 sudo hostnamectl set-hostname --static fedora;
+
+# Install languages
+
+sudo dnf install golang;
