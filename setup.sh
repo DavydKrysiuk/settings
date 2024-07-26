b51bcde2ccaf30b48fcc799b970103cf6f056f8c
Davyd Krysiuk <davydkrysiuk@gmail.com>
2024-07-26 23:17:58 +0100

    Update setup.sh for auto setting of hostname

--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
 sudo dnf remove firefox gnome-tour gnome-contacts gnome-maps gnome-calendar gnome-connections gnome-weather libreoffice-calc libreoffice-impress libreoffice-writer libreoffice-core;
+sudo hostnamectl set-hostname --static fedora;
