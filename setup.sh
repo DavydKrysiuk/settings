fa6b275145a24eabdf300d4f4e59c5e52f34fc7c
Davyd Krysiuk <davydkrysiuk@gmail.com>
2024-07-27 01:57:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 sudo dnf remove firefox gnome-tour gnome-contacts gnome-maps gnome-calendar gnome-connections gnome-weather libreoffice-calc libreoffice-impress libreoffice-writer libreoffice-core;
 sudo hostnamectl set-hostname --static fedora;
+sudo rm -d -r .mozilla
 
 # Install languages
 
