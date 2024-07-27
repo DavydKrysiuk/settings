166e08cdf5848728f162f71cc39c97c9836155d0
Davyd Krysiuk <davydkrysiuk@gmail.com>
2024-07-27 06:36:05 +0100

    Update emacs-garbage.el

--- emacs-garbage.el
+++ emacs-garbage.el
@@ -1,4 +1,18 @@
-;; 'gruber-darker-theme 'go-mode 'magit
+;; 'gruber-darker-theme 'go-mode 'haskell-mode 'magit
+
+(require 'package)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives
+   (quote
+    (("gnu" . "https://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")))))
+(package-initialize)
+;; M-x package-refresh-contents
+;; M-x package-install RET haskell-mode
 
 (set-frame-font "Mono 16" nil t)
 
