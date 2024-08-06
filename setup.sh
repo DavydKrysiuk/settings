dd09a82e8cb0f841ef4b39b250232c8e83059110
Davyd Krysiuk <davydkrysiuk@gmail.com>
2024-08-06 02:57:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
-# Remove all preinstalled dogshit
+## Remove all preinstalled dogshit
 
 sudo dnf remove firefox gnome-tour gnome-contacts gnome-maps gnome-calendar gnome-connections gnome-weather libreoffice-calc libreoffice-impress libreoffice-writer libreoffice-core;
 
+## Set host name (obviously)
 sudo hostnamectl set-hostname --static fedora;
 
-# Install languages
-# Go
+## Install languages
 
+
+# Go
 sudo dnf install golang;
 mkdir -p $HOME/go
 echo 'export GOPATH=$HOME/go' >> $HOME/.bashrc
@@ -16,27 +18,30 @@ curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/insta
 curl -sSfL https://raw.githubusercontent.com/air-verse/air/master/install.sh | sh -s
 
 # Assembly
-
 sudo dnf install nasm
 
 # C/C++
-
 sudo dnf install clang
 
-# Haskell
+# Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+
+# Haskell
 sudo dnf install haskell-platform
 
-# Install tools
+## Install tools
+
+curl -o google-chrome.rpm https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm 
+sudo dnf install ./google-chrome.rpm # not sure about this line
+rm google-chrome.rpm
 
 # https://youtu.be/PKaJoqQQoIA?si=JDbt_pdjAz2TNZtu
 sudo dnf install emacs;
 git config --global core.editor emacs
 
-sudo dnf upgrade git;
-
 # Setup git
-
+sudo dnf upgrade git;
 git config --global init.defaultBranch main
 echo Setup git? [y/n]
 read setupgit
@@ -51,6 +56,5 @@ then
 else
   echo Git setup skipped
 fi
-
 
 sudo rm -d -r .mozilla;
